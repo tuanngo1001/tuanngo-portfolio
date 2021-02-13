1fa836058e634fb3d78bc06fc354af5f71904f89
Tuan Ngo <anhtuanngo101@gmail.com>
2021-02-13 04:08:06 -0600

    Updates

---
+++ precache-manifest.80b75b2e1cc088d3aeff24be2d330e8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f712be3c4698ba856f13471ea3335e33",
+    "url": "/tuanngo-portfolio/index.html"
+  },
+  {
+    "revision": "0863daf4451350e5b8da",
+    "url": "/tuanngo-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "65be7de0c322869c8b58",
+    "url": "/tuanngo-portfolio/static/css/main.40f0e6cc.chunk.css"
+  },
+  {
+    "revision": "0863daf4451350e5b8da",
+    "url": "/tuanngo-portfolio/static/js/2.4e674235.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/tuanngo-portfolio/static/js/2.4e674235.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65be7de0c322869c8b58",
+    "url": "/tuanngo-portfolio/static/js/main.f8d00cf4.chunk.js"
+  },
+  {
+    "revision": "aabdd1f18b475a27cb76",
+    "url": "/tuanngo-portfolio/static/js/runtime-main.6638543a.js"
+  }
+]);
